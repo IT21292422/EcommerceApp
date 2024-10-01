b7c7e349996e6d7247cec831b2966ae42db560b3
chamath-shanaka <chamathshanaka19@gmail.com>
2024-10-02 02:25:25 +0530

    Login UI

---
+++ app/src/main/java/com/example/ecommerce/activity/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ecommerce.activity
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecommerce.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var username : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            // TODO: implement log in logic
+        })
+
+        binding.switchToRegisterButton.setOnClickListener(View.OnClickListener {
+            // TODO: implement switch to register screen logic
+        })
+    }
+}
