ed4812b6b235ab2347128369a985db015511d5a5
chamath-shanaka <chamathshanaka19@gmail.com>
2024-10-03 19:39:23 +0530

    Oder status UI

---
+++ app/src/main/java/com/example/ecommerce/activity/OrderStatusActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ecommerce.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerce.R
+
+class OrderStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_status)
+    }
+}
