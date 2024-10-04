93e114f3b54afe0c60e34a08e2e9d6d101c08cdb
chamath-shanaka <chamathshanaka19@gmail.com>
2024-10-04 16:51:41 +0530

    Rating UIs

---
+++ app/src/main/java/com/example/ecommerce/activity/VendorRatingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ecommerce.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerce.R
+
+class VendorRatingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendor_ratings)
+    }
+}
