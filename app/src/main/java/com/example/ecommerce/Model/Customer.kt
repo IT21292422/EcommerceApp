82399cd1220946153191ea93e9bc152b8532e4e3
chamath-shanaka <chamathshanaka19@gmail.com>
2024-10-08 19:56:50 +0530

    customer profile view logic without api

---
+++ app/src/main/java/com/example/ecommerce/Model/Customer.kt
@@ -0,0 +1,8 @@
+package com.example.ecommerce.Model
+
+data class Customer(
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val creationDate: String
+)
--- app/src/main/java/com/example/ecommerce/activity/CustomerProfileActivity.kt
+++ app/src/main/java/com/example/ecommerce/activity/CustomerProfileActivity.kt
@@ -1,16 +1,43 @@
 package com.example.ecommerce.activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ecommerce.databinding.ActivityCustomerProfileBinding
-import com.example.ecommerce.databinding.ActivityLoginBinding
+import com.example.ecommerce.Model.Customer
+import com.example.ecommerce.R
+import okhttp3.*
 
 class CustomerProfileActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCustomerProfileBinding
+    private lateinit var tvEmail: TextView
+    private lateinit var tvFirstName: TextView
+    private lateinit var tvLastName: TextView
+    private lateinit var tvCreationDate: TextView
+    private lateinit var btnEdit: Button
+    private lateinit var btnDeactivate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCustomerProfileBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_customer_profile)
+
+        tvEmail = findViewById(R.id.tvEmail)
+        tvFirstName = findViewById(R.id.tvFirstName)
+        tvLastName = findViewById(R.id.tvLastName)
+        tvCreationDate = findViewById(R.id.tvCreationDate)
+        btnEdit = findViewById(R.id.btnEdit)
+        btnDeactivate = findViewById(R.id.btnDeactivate)
+
+        fetchCustomerProfile()
+    }
+
+    private fun fetchCustomerProfile() {
+        // TODO
+    }
+
+    private fun updateUI(customer: Customer) {
+        tvEmail.text = "Email: ${customer.email}"
+        tvFirstName.text = "First Name: ${customer.firstName}"
+        tvLastName.text = "Last Name: ${customer.lastName}"
+        tvCreationDate.text = "Created on: ${customer.creationDate}"
     }
 }
